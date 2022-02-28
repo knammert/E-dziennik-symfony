c86440e0878f9b3207371d92cb553b49942b3212
knammert <knammert@gmail.com>
2022-02-28 12:11:15 +0100

    UserEdit

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -2,15 +2,18 @@
 
 namespace App\Controller;
 
+use App\Repository\ClassNamesRepository;
 use App\Repository\UsersRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UsersController extends AbstractController
 {
-    public function __construct(EntityManagerInterface $em, UsersRepository $usersRepository){
+    public function __construct(EntityManagerInterface $em, ClassNamesRepository $classNamesRepository, UsersRepository $usersRepository){
+        $this->classNamesRepository = $classNamesRepository;
         $this->usersRepository = $usersRepository;
         $this->em = $em;     
     }
@@ -20,9 +23,25 @@ class UsersController extends AbstractController
     {
 
         $users = $this->usersRepository->findAll();
+        $classes = $this->classNamesRepository->findAll();
 
         return $this->render('/adminPanel/users/index.html.twig', [
             'users' => $users,
+            'classes' => $classes,
         ]);
     }
+
+    #[Route('/adminPanel/index/update/{userId}',methods:['POST','GET','PUT'], name: 'user_update')]
+    public function update($userId, Request $request){      
+
+         $user = $this->usersRepository->find($userId);
+         $class =  $this->classNamesRepository->find($request->get('className'));
+         $user->setClassName($class);
+         $user->setRoles([$request->get('role')]);
+         $this->em->flush();
+        
+         return $this->redirect($request->headers->get('referer'));
+    }
+
+
 }
